06c69c39d8d082afa6da118761116ed54e808b52
Alexander <leyderin@unicauca.edu.co>
2024-03-11 11:06:09 -0500

    Implementación restController

--- src/main/java/co/edu/unicauca/asae/asst/batteries/asstbatteries/layerControllers/BatteryRestController.java
+++ src/main/java/co/edu/unicauca/asae/asst/batteries/asstbatteries/layerControllers/BatteryRestController.java
@@ -1,6 +1,8 @@
 package co.edu.unicauca.asae.asst.batteries.asstbatteries.layerControllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -8,14 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.unicauca.asae.asst.batteries.asstbatteries.facadeServices.DTO.BatteryDTO;
 import co.edu.unicauca.asae.asst.batteries.asstbatteries.facadeServices.services.IBatteryService;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-
 
 @RestController
 @RequestMapping("/api")
@@ -44,10 +45,19 @@ public class BatteryRestController {
         return this.batteryService.update(id, battery);
     }
 
-    @DeleteMapping("battery/{id}")
-    public Boolean delete(@PathVariable Long id) {
-        return this.batteryService.delete(id);
-        
+    @DeleteMapping("battery")
+    public Map<String, Object> delete(@RequestParam Long id) {
+       
+        Map<String, Object> response = new HashMap<>();
+        String message = null;
+        if(this.batteryService.delete(id)) {
+            message = "La bateria con id: " + id +  " se elimino con exito";
+        }
+        else {
+            message = "La bateria ya se encuentra eliminada o no se encuentra registrada con el id: " + id;
+        }
+        response.put("respuesta", message);
+        return response;
     }
 
 }
